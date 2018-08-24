c99d63b27b2ae1117777ffe2b42834963ea1189b
sbx_user1059 <sbx_user1059@ip-10-129-97-141.eu-west-2.compute.internal>
2018-08-24 11:25:50 +0000

    Fri Aug 24 11:25:50 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,11 +1,9 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        return "1";
     }
 
 }
--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,5 +1,16 @@
 package befaster.solutions.FIZ;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzSolutionTest
 {
+	private FizzBuzzSolution solution = new FizzBuzzSolution();
+
+	@Test
+	public void oneReturnsOne()
+	{
+		assertEquals("1", solution.fizzBuzz(1));
+	}
 }
